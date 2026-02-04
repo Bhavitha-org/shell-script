2b74f8aef3d78fa7742cf7670a7eec3169ac0e86
Bhavitha-Lakkireddy <bhavii342@gmail.com>
2026-02-04 14:41:59 +0530

    variables

--- variables1.sh
+++ variables1.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "Enter your username"
-read USERNAME
+# echo "Enter your username"
+# read USERNAME
 
-echo "your username is $USERNAME"
+# echo "your username is $USERNAME"
 
-echo "Enter your password"
-read -s PASSWORD              #-s hiding password while entering
+# echo "Enter your password"
+# read -s PASSWORD              #-s hiding password while entering
+
+TIME_STAMP=$(date)
+
+echo "command executed at $TIME_STAMP"
+
 
