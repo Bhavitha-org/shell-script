47f06bbae9d14bac437f2b2423efdc86d984d7a9
Bhavitha-Lakkireddy <bhavii342@gmail.com>
2026-02-04 15:11:05 +0530

    variables

--- special_variables.sh
+++ special_variables.sh
@@ -9,4 +9,4 @@ echo "Home directory of current user: $HOME"
 echo "PID of this script: $$"
 sleep 100 &
 echo "PID of recently executed background process: $!"
-echo "All args passed to script: $*"   #it makes all args combine so use $@
+echo "All args passed to script: $*"   #it treats all args as single arg, so use $@
