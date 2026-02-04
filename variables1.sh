b3ba03677982193a6d4c18cceb8e3482f327aea1
Bhavitha-Lakkireddy <bhavii342@gmail.com>
2026-02-04 14:19:10 +0530

    variables

---
+++ variables1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Enter your username"
+read USERNAME
+
+echo "your username is $USERNAME"
+
+echo "Enter your password"
+read -s PASSWORD              #-s hiding password while entering
+
