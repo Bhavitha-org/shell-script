f7992e16621f29874ea2534022bb739cdf4ff80c
Bhavitha-Lakkireddy <bhavii342@gmail.com>
2026-02-04 15:35:55 +0530

    arrays

---
+++ Arrays1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("Apple", "Banana", "Guava")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "Fruit 1: ${FRUITS[0]}"
+echo "Fruit 2: ${FRUITS[1]}"
+echo "Fruit 3: ${FRUITS[2]}"
--- datatypes1.sh
+++ datatypes1.sh
@@ -5,3 +5,6 @@ NUM2=200
 
 SUM=$(($NUM1+$NUM2))
 echo "sum is $SUM"
+
+#there is no much importance to datatypes in shell-scripting, 
+#even if we give a string in NUM2, it will return 100, taking string as 0.
