2e157321cd3a3b32713cb5c4e4bee21c228f1b5f
Bhavitha-Lakkireddy <bhavii342@gmail.com>
2026-02-04 22:56:52 +0530

    logs

--- log-redirections.sh
+++ log-redirections.sh
@@ -5,16 +5,16 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "please run this script with root user access"
+    echo "please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE() {
 if [ $1 -ne 0 ]; then
-    echo "$2 is...Failure"
+    echo "$2 is...FAILURE" | tee -a $LOGS_FILE   #failure err will be on screen but log file has no VISIBLE err, so to print on both screen and file we use 'tee'
     exit 1
 else 
-    echo "$2 is...Success"
+    echo "$2 is...SUCCESS" | tee -a $LOGS_FILE     #-a is append 
 fi
 }
 
