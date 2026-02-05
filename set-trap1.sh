d740f1f38ae693a8d6fddc3f85c17041aab0ea3f
Bhavitha-Lakkireddy <bhavii342@gmail.com>
2026-02-05 11:55:24 +0530

    set-trap

--- set-trap1.sh
+++ set-trap1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e          #whenever set -e feels an error it will send signal to ERR
+                #trap 'Commands to run' ERR -> trap syntax
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR       #$LINENO, $BASH_COMMAND are default variables
+
 echo "Hello world"
 echo "this is set-trap script"
 echoo "there is error in this line"
