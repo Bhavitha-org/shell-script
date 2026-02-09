27217a0ade5433fc4162e048a48a89c91607cbc4
Bhavitha <lbhavitha342@gmail.com>
2026-02-09 23:09:48 +0530

    del log script

--- whileloop.sh
+++ whileloop.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-count=1
+# count=1
 
-while [ $count -le 5 ] 
+# while [ $count -le 5 ] 
+# do
+#     echo "Count is $count"
+#     sleep 1
+#     ((count++)) 
+# done
+
+while IFS= read -r line; #Internal field seperator, reads lines in file and 
 do
-    echo "Count is $count"
-    sleep 1
-    ((count++)) 
-done
+    echo "$line" #prints each line.
+done < script1.sh 
