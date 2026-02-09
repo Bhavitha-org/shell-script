eddecbc8bef4e626efd193054ce13a34160a1f6a
Bhavitha <lbhavitha342@gmail.com>
2026-02-09 21:14:11 +0530

    del log script

---
+++ delete-old-logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$LOGS_DIR=/home/ec2-user/app-logs
+$LOGS_FILE="$LOGS_DIR/$0.log"
+
+if [ ! -d $LOGS_DIR ]; then
+    echo "$LOGS_DIR doesn't exist"
+
+FILES_TO_DELETe=$(find $LOGS_DIR -name "*.log" -mtime +14)
+echo "$FILES_TO_DELETE"
