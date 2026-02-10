06bb940e610a3135e281deba22a6399216330991
Bhavitha <lbhavitha342@gmail.com>
2026-02-10 13:13:32 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -9,6 +9,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} #14days is the default value when user doesn't pass arg
+
 if [ $USERID -ne 0 ]; then
     echo -e " $R please run this script with root user access $N"
 fi
@@ -23,3 +27,13 @@ USAGE() {
 if [ $# -lt 2 ]; then
     USAGE
 fi 
+
+if [ ! -d SOURCE_DIR ]; then
+    echo -e "$R $SOURCE_DIR doesn't exist $N"
+    exit 1
+fi
+
+if [ ! -d DEST_DIR ]; then
+    echo -e "$R $DEST_DIR doesn't exist $N"
+    exit 1
+fi
