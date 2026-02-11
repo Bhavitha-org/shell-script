a9cb629b7c4906091693f0a5388ee389fc466ca0
Bhavitha <lbhavitha342@gmail.com>
2026-02-11 16:25:41 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -63,111 +63,25 @@ else
     TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
     echo "Archieve name: $ZIP_FILE_NAME"
+    find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS | tar -zcvf $ZIP_FILE_NAME
+
+    #check archieve is success or not
+    if [ -f $ZIP_FILE_NAME ]; then
+        log "Archeival is...$G SUCCESS $N"
+        #if archieval is success, then delete logs in app-logs
+        while IFS= read -r filepath;
+        do 
+            echo "Deleting file: $filepath"
+            
+            rm -f "$filepath"
+            echo "Deleted file: $filepath"
+        done <<< "$FILES"
+    else 
+        log "Archeival is...$R FAILURE $N"
+        exit 1
+    fi
 fi
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# USERID=$(id -u)
-# LOGS_FOLDER="/var/log/shell-script"
-# LOGS_FILE="/var/log/shell-script/backup.log"
-
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[0m"
-
-# SOURCE_DIR=$1
-# DEST_DIR=$2
-# DAYS=${3:-14} #14days is the default value when user doesn't pass arg
-
-# log(){
-#     log -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
-# }
-
-# if [ $USERID -ne 0 ]; then
-#     log -e " $R please run this script with root user access $N"
-# fi
-
-# mkdir -p $LOGS_FOLDER
-
-# USAGE() {
-#     log -e "$R USAGE: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
-#     exit 1
-# }
-
-# if [ $# -lt 2 ]; then
-#     USAGE
-# fi 
-
-# if [ ! -d $SOURCE_DIR ]; then
-#     log -e "$R Source Directory: $SOURCE_DIR doesn't exist $N"
-#     exit 1
-# fi
-
-# if [ ! -d $DEST_DIR ]; then
-#     log -e "$R Destination Directory: $DEST_DIR doesn't exist $N"
-#     exit 1
-# fi
-
-# #find the files
-
-# FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
-
-# log "Backup started" #log is a function, Backup started->$1
-# log "Source Directory: $SOURCE_DIR"
-# log "Destination Directory: $DEST_DIR"
-# log "Days: $DAYS" 
-
-# if [ -z "${FILES}" ]; then        #checking variable empty or not
-#     log "No files to archieve...$Y SKIPPING $N"
-# else
-#     #app-logs-$timestamp.zip
-#     log "Files found to archieve: $FILES"
-#     TIMESTAMP=$(date +%F-%H-%M-%S)
-#     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
-#     log "Archieve name: $ZIP_FILE_NAME"
-#     tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)       #output of find, to input of tar
-
-#     #check archieve is success or not
-#     if [ -f $ZIP_FILE_NAME ]; then
-#         log "Archeival is...$G SUCCESS $N"
-#         while IFS= read -r filepath;
-#             do 
-#                 log "Deleting file: $filepath"
-                
-#                 rm -f $filepath
-#                 log "Deleted file: $filepath"
-#             done <<< $FILES_TO_DELETE
-#     else
-#         log "Archeival is...$R FAILURE $N"
-#         exit 1
-#     fi
-# fi
 
 
 # 1. user should pass source_dir and dest_dir, default is 14 days, but user can override
@@ -175,4 +89,4 @@ fi
 # 3. find the files and zip them.
 # 4. archieve them and place into dest_dir
 # 5. check archeive is success or not
-# 6. you can delete from source_dir
+# 6. if success, you can delete from source_dir
